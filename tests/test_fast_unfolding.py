14583d09bd4069b03e97883487f1adbe05185078
shaomingyang <954260135@qq.com>
2020-11-17 21:37:45 +0800

    增加测试

--- tests/test_fast_unfolding.py
+++ tests/test_fast_unfolding.py
@@ -13,6 +13,16 @@ class TestFastUnfolding(unittest.TestCase):
         G = Graph({1: {2: 1, 4: 1, 7: 1}, 2: {1: 1, 0: 1, 4: 1, 6: 1}, 4: {1: 1, 2: 1, 10: 1}, 7: {1: 1, 3: 1, 5: 1, 6: 1}, 0: {2: 1, 3: 1, 5: 1}, 6: {2: 1, 7: 1, 11: 1}, 3: {0: 1, 7: 1}, 5: {0: 1, 7: 1, 11: 1}, 10: {4: 1, 11: 1, 14: 1, 13: 1}, 11: {6: 1, 5: 1, 10: 1}, 14: {10: 1, 8: 1, 9: 1}, 13: {10: 1}, 8: {15: 1, 14: 1, 9: 1}, 15: {8: 1}, 9: {8: 1, 14: 1, 12: 1}, 12: {9: 1}}, symmetric=False)
         communities = fu.process(G)
         self.assertEqual(communities, [[1, 2, 4, 7, 0, 6, 3, 5], [10, 11, 13], [14, 8, 9, 15, 12]])
+
+        G = Graph({0: [], 1: [], 2:[]})
+        fu = FastUnfolding()
+        communities = fu.process(G)
+        self.assertEqual(communities, [[0],[1], [2]])
+
+        G = Graph({0: {1:1}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}})
+        fu = FastUnfolding()
+        communities = fu.process(G)
+        self.assertEqual(communities, [[0,1],[2],[3],[4],[5],[6],[7]])
         
     def test_modularity(self):
         G = Graph({0: [1, 2], 1: [2]})
@@ -24,6 +34,19 @@ class TestFastUnfolding(unittest.TestCase):
         communities = fu.process(G)
         Q = fu.modularity(G, communities)
         self.assertEqual(Q, 0.3998)
+        #成为各个孤立点时，不能算模块度
+        G = Graph({0: [], 1: [], 2:[]})
+        fu = FastUnfolding()
+        communities = fu.process(G)
+        Q = fu.modularity(G, communities)
+        self.assertEqual(Q, 0.0)
+    
+        G = Graph({0: {1:1}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}})
+        fu = FastUnfolding()
+        communities = fu.process(G)
+        Q = fu.modularity(G, communities)
+        self.assertEqual(Q, 0.0)
+
         
         
 if __name__ == '__main__':
